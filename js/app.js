664ab5468064da826bd4bb81a7825275d5eb5932
Joni Weiss <hi@joniwebgirl.com>
2015-07-24 21:02:31 -0600

    added copyright info to footer. new app.js file

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/**
+ * Created by joniweiss on 7/24/15.
+ */
+$(document).ready(function() {
+    var thisYear = new Date().getFullYear();
+    var timeNow  = new Date().getTime();
+
+    var jcopyright = " Copyright 2009 - " + thisYear + ", Jitterbug Coffee Hop, Salt Lake City, Utah";
+
+    $("#copyright").find("p").addClass("fa fa-copyright").append(jcopyright);
+});

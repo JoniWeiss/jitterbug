5401d815b73d2ac2f20780d960e797f4f1c81b13
Joni Weiss <hi@joniwebgirl.com>
2015-08-09 22:37:52 -0600

    converted to single-page angular app

--- js/app.js
+++ js/app.js
@@ -10,3 +10,33 @@ $(document).ready(function() {
     $("#copyright").find("p").addClass("fa fa-copyright").append(jcopyright);
 
 });
+
+var app=angular.module('jitterbug-app',['ngRoute']);
+
+
+app.config(function($routeProvider){
+      $routeProvider
+          .when('/',{
+                templateUrl: '/templates/home.html'
+          })
+          .when('/beveragesMenu',{
+                templateUrl: '/templates/beverages.html'
+          })
+          .when('/foodMenu',{
+                templateUrl: '/templates/food.html'
+          })
+          .when('/mapToUs',{
+                templateUrl: '/templates/map.html'
+          })
+          .when('/aboutUs',{
+            //   <a href="tel:801-884-7450"></a>
+                templateUrl: '/templates/about.html'
+          })
+});
+
+
+app.controller('cfgController',function($scope){
+
+      $scope.message="Hello world";
+
+});

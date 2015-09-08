4c74dc5f3ee6b6112ce06c2ca45e9948a9d94264
Joni Weiss <hi@joniwebgirl.com>
2015-09-08 13:06:15 -0600

    converted video to videogular
    unwatched video files

--- js/app.js
+++ js/app.js
@@ -158,11 +158,14 @@ app.controller('DefaultCtrl', ['$scope', 'smoothScroll',
 ]);
 
 app.controller('HomeCtrl', ['$scope', 'smoothScroll', '$sce', function ($scope, smoothScroll, $sce) {
-        this.config = {
+    var prePath = "//joniwebgirl.com/jitterbug.com/assets"; // production
+    //var prePath = "assets"; // dev/test - github doesn't allow large file sizes
+
+    this.config = {
             sources: [
-                {src: $sce.trustAsResourceUrl("assets/jitterbug-commercial-2015-08.mp4"), type: "video/mp4"},
-                {src: $sce.trustAsResourceUrl("assets/jitterbug-commercial-2015-08.webm"), type: "video/webm"},
-                {src: $sce.trustAsResourceUrl("assets/jitterbug-commercial-2015-08.ogg"), type: "video/ogg"}
+                {src: $sce.trustAsResourceUrl(prePath + "/jitterbug-commercial-2015-08.mp4"), type: "video/mp4"},
+                {src: $sce.trustAsResourceUrl(prePath + "/jitterbug-commercial-2015-08.webm"), type: "video/webm"},
+                {src: $sce.trustAsResourceUrl(prePath + "/jitterbug-commercial-2015-08.ogg"), type: "video/ogg"}
             ],
             tracks: [
                 {
